0b8ce5420849fc5fb6dd8260be48a21df65fa03b
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-09-06 18:30:10 +0530

    Minor fix

--- project/hooks/useFrameworkReady.ts
+++ project/hooks/useFrameworkReady.ts
@@ -1,5 +1,12 @@
 import { useEffect } from 'react';
 
+// Extend the Window interface to include frameworkReady
+declare global {
+  interface Window {
+    frameworkReady?: () => void;
+  }
+}
+
 export function useFrameworkReady() {
   useEffect(() => {
     // Signal that the framework is ready
